7e5965a2926a5a65b9847dda512df4351c547cd8
ahmedgfouad <aghmedgfouad2020@gmail.com>
2023-03-21 17:16:01 +0200

    make the project

---
+++ lib/ex.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Ex extends StatelessWidget {
+  const Ex({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
